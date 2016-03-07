65b308cbd7a1e6f2232e19dacb75cf4efe2f2444
Hassan Karaouni <hkII@Hassans-MacBook-Pro-2.local>
2016-03-06 22:30:58 -0800

    Gaze freezes bear

---
+++ Assets/GazeTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		print("You looked at me!");
+	}
+
+	void OnTriggerExit(Collider col) {
+		print ("You stopped looking!");
+	}
+}
--- Assets/Scripts/ToyMovement.cs
+++ Assets/Scripts/ToyMovement.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 
 public class ToyMovement : MonoBehaviour {
-
 	public GameObject bear;
 	public GameObject bear_positions;
 	public Light final_light;
+
+	private bool canMove;
 
 	// Use this for initialization
 	void Start () {
 		bear = GameObject.Find ("Teddy Main");
 		bear_positions = GameObject.Find ("Bear Positions");
 		final_light.intensity = 0.0f;
+		canMove = false;
 
 		StartCoroutine (moveBear ());
 	}
@@ -22,17 +24,34 @@ public class ToyMovement : MonoBehaviour {
 
 	private IEnumerator moveBear() {
 		yield return new WaitForSeconds (19.0f);
+		while (!canMove) {
+			yield return new WaitForSeconds (.1f);
+		}
 		bear.transform.position = bear_positions.transform.GetChild (0).gameObject.transform.position;
 
 		yield return new WaitForSeconds (5.0f);
+		while (!canMove) {
+			yield return new WaitForSeconds (.1f);
+		}
 		bear.transform.position = bear_positions.transform.GetChild (1).gameObject.transform.position;
 
 		yield return new WaitForSeconds (5.0f);
+		while (!canMove) {
+			yield return new WaitForSeconds (.1f);
+		}
 		bear.transform.position = bear_positions.transform.GetChild (2).gameObject.transform.position;
 		bear.transform.rotation = bear_positions.transform.GetChild (2).gameObject.transform.rotation;
 		final_light.intensity = 30.0f;
 		GetComponent<AudioSource> ().Play ();
-	}	
+	}
+
+	void OnTriggerEnter() {
+		canMove = false;
+	}
+
+	void OnTriggerExit() {
+		canMove = true;
+	}
 }
 
 
