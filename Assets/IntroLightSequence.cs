9954792286be2bbef0f5ebda8ff1ab57be9ad3a1
Hassan Karaouni <hkII@DN0a233239.SUNet>
2016-03-06 18:06:56 -0800

    Commit first light flickers

---
+++ Assets/IntroLightSequence.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroLightSequence : MonoBehaviour {
+	public Light light;
+	public Light light1;
+	public Light light2;
+
+	public GameObject musicBox;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (lightsOff ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator lightsOff() {
+		yield return new WaitForSeconds(15.0f);
+
+		Destroy (musicBox);
+		GetComponent<AudioSource> ().Play ();
+		Destroy (light.gameObject);
+
+		yield return new WaitForSeconds (0.4f);
+		Destroy (light1.gameObject);
+
+		yield return new WaitForSeconds (0.3f);
+		Destroy (light2.gameObject);
+	}
+}
---
+++ Assets/Scripts/ToyMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToyMovement : MonoBehaviour {
+
+	public GameObject bear;
+	public GameObject bear_positions;
+	public int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		bear = GameObject.Find ("Teddy Main");
+		bear_positions = GameObject.Find ("Bear Positions");
+
+		GameObject bear_position1 = bear_positions.transform.GetChild (0).gameObject;
+		GameObject bear_position2 = bear_positions.transform.GetChild (1).gameObject;
+		GameObject bear_position3 = bear_positions.transform.GetChild (2).gameObject;
+
+		StartCoroutine (moveBear ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private IEnumerator moveBear() {
+		yield return new WaitForSeconds (19.0f);
+		bear.transform.position = bear_positions.transform.GetChild (count).gameObject.transform.position;
+		count++;
+
+		yield return new WaitForSeconds (5.0f);
+		bear.transform.position = bear_positions.transform.GetChild (count).gameObject.transform.position;
+		count++;
+
+		yield return new WaitForSeconds (5.0f);
+		bear.transform.position = bear_positions.transform.GetChild (count).gameObject.transform.position;
+		count++;
+	}	
+}
+
+
+
