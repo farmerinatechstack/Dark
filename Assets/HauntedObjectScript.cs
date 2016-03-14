aa6e19cdbaf5865b1a53175937a0cc2e38953137
Hassan Karaouni <hkII@DN0a22b3a2.SUNet>
2016-03-13 22:39:14 -0700

    Add semi-complete Lesson 1

--- Assets/HauntedObjectScript.cs
+++ Assets/HauntedObjectScript.cs
@@ -5,23 +5,102 @@ public class HauntedObjectScript : MonoBehaviour {
 	public bool canMove;
 	public bool isHaunted;
 
-	// store tiers
+	public GameObject[] tiers;
 
 	public AudioClip warningClip;
 	public AudioClip killClip;
 
-	private float tier3Delay;
-	private float tier2Delay;
+	private int currentTier;
+
 	private float tier1Delay;
+	private float tier2Delay;
+	private float tier3Delay;
 
 	// Use this for initialization
 	void Start () {
 		canMove = false;
 		isHaunted = false;
+		currentTier = 0;
+
+		tier1Delay = Random.Range(3.0f, 7.0f);
+		tier2Delay = Random.Range(3.0f, 7.0f);
+		tier3Delay = Random.Range(3.0f, 7.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	public void StartHaunt() {
+		Debug.Log ("starting haunt");
+		canMove = true;
+		isHaunted = true;
+		StartCoroutine (MoveAcrossTiers());
+	}
+
+	IEnumerator MoveAcrossTiers() {
+		yield return new WaitForSeconds (tier1Delay);
+		while (!canMove)
+			yield return new WaitForSeconds (0.1f);
+		GameObject tier = tiers [currentTier];
+		int tierIndex = Random.Range (0, tier.transform.childCount);
+		GameObject child = tier.transform.GetChild (tierIndex).gameObject;
+		while (!child.GetComponent<LocationScript> ().isFree) {
+			tierIndex = Random.Range (0, tier.transform.childCount);
+			print ("tierIndex:" + tierIndex);
+			yield return new WaitForSeconds (2.0f);
+		}
+		gameObject.transform.position = tier.transform.GetChild (tierIndex).position;
+		currentTier++;
+
+		yield return new WaitForSeconds (tier2Delay);
+		while (!canMove)
+			yield return new WaitForSeconds (0.1f);
+		tier = tiers [currentTier];
+		tierIndex = Random.Range (0, tier.transform.childCount);
+		child = tier.transform.GetChild (tierIndex).gameObject;
+		while (!child.GetComponent<LocationScript> ().isFree) {
+			tierIndex = Random.Range (0, tier.transform.childCount);
+			yield return new WaitForSeconds (2.0f);
+		}
+		gameObject.transform.position = tier.transform.GetChild (tierIndex).transform.position;
+		currentTier++;
+
+		yield return new WaitForSeconds (tier3Delay);
+		while (!canMove)
+			yield return new WaitForSeconds (0.1f);
+		tier = tiers [currentTier];
+		tierIndex = Random.Range (0, tier.transform.childCount);
+		child = tier.transform.GetChild (tierIndex).gameObject;
+		while (!child.GetComponent<LocationScript> ().isFree) {
+			tierIndex = Random.Range (0, tier.transform.childCount);
+			yield return new WaitForSeconds (2.0f);
+		}
+		gameObject.transform.position = tier.transform.GetChild (tierIndex).transform.position;
+		currentTier++;
+	}
+
+	void MoveToNextTier() {
+		GameObject tier = tiers [currentTier];
+		int tierIndex = Random.Range (0, tier.transform.childCount);
+		GameObject child = tier.transform.GetChild (tierIndex).gameObject;
+		while (!child.GetComponent<LocationScript> ().isFree) {
+			tierIndex = Random.Range (0, tier.transform.childCount);
+		}
+		gameObject.transform.position = tier.transform.GetChild (tierIndex).transform.position;
+		currentTier++;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.name == "GazeArea") {
+			canMove = false;
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.name == "GazeArea") {
+			canMove = true;
+		}
 	}
 }
--- Assets/Scripts/TimeEventManager.cs
+++ Assets/Scripts/TimeEventManager.cs
@@ -5,6 +5,8 @@ public class TimeEventManager : MonoBehaviour {
 	// Time Delays
 	public static float TIME_BEFORE_INTRO = 2.0f;
 
+	public GameObject[] hauntedObjects;
+
 	// Delegate & Event System
 	public delegate void TimeAction();
 	public static event TimeAction GameStarted;
@@ -33,7 +35,8 @@ public class TimeEventManager : MonoBehaviour {
 		yield return new WaitForSeconds (TIME_BEFORE_INTRO);
 		if (GameStarted != null)
 			GameStarted ();
-		
+
+
 		ambientSrc.Play ();
 		while (!LampFlicker.CompletedFlicker) {
 			yield return new WaitForSeconds (0.1f);
@@ -48,5 +51,28 @@ public class TimeEventManager : MonoBehaviour {
 		yield return new WaitForSeconds (1.0f);
 		if (InstructionsDone != null)
 			InstructionsDone ();
+		
+
+		for (int i = 0; i < hauntedObjects.Length; i++) {
+			GameObject hauntedObj = hauntedObjects[i];
+			print (hauntedObj.name);
+			hauntedObj.GetComponent<HauntedObjectScript> ().StartHaunt ();
+		}
+
+		/*
+		int numObjects = hauntedObjects.Length;
+		while (numObjects > 0) {
+			print ("in while loop");
+			int randObjIndex = Random.Range (0, hauntedObjects.Length);
+			GameObject hauntedObj = hauntedObjects[randObjIndex];
+			if (hauntedObj != null) {
+				//syield return new WaitForSeconds (Random.Range (0.1f, 2.0f));
+				print (hauntedObj.name);
+				hauntedObj.GetComponent<HauntedObjectScript> ().StartHaunt ();
+				hauntedObjects [randObjIndex] = null;
+				numObjects--;
+			}
+		}
+		*/
 	}
 }
